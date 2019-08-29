aab35ed59b5dcdd6109ceeb8b6c2098f5f85e770
刘江 <2293148052@qq.com>
2019-08-29 20:08:10 +0800

    完成网站设置数据和登录用户信息同步

--- alibaixiu/public/assets/js/common.js
+++ alibaixiu/public/assets/js/common.js
@@ -13,4 +13,21 @@ $('#logout').on('click', function() {
             }
         })
     }
+});
+
+// 处理日期时间格式
+function formateDate(date) {
+    // 将日期时间字符串转换成日期对象
+    date = new Date(date);
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+}
+
+// 向服务器端发送请求 索要登录用户信息
+$.ajax({
+    type: 'get',
+    url: '/users/' + userId,
+    success: function(response) {
+        $('.avatar').attr('src', response.avatar)
+        $('.profile .name').html(response.nickName)
+    }
 })
--- alibaixiu/routes/actions/other/loginStatus.js
+++ alibaixiu/routes/actions/other/loginStatus.js
@@ -1,7 +1,8 @@
-module.exports = async (req, res) => {
-	if (req.session && req.session.userInfo) {
-		res.send('var isLogin = true')
-	}else {
-		res.send('var isLogin = false')
-	}
+module.exports = async(req, res) => {
+    if (req.session && req.session.userInfo) {
+        const s = `var isLogin = true; var userId=\"${req.session.userInfo._id}\"`
+        res.send(s)
+    } else {
+        res.send('var isLogin = false')
+    }
 };
